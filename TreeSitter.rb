f1a63341b447c1d3d38ac333492f11de4ea5d275
Joichiro Okoshi <okoshi811@gmail.com>
2024-10-03 18:07:51 +0900

    add: ノードから型(primitive_typeのみ)と変数名を取り出せるように

--- TreeSitter.rb
+++ TreeSitter.rb
@@ -4,22 +4,38 @@ require 'tree_sitter'
 # get path for parser from environmental variables
 PATH_TO_PARSER = ENV['PATH_TO_C99PARSER']
 
-# check child node of :compound_statement's
-def searchCompoundStatement(node)
+
+# 3rd depth
+# check :declaration (variable declaration)
+def searchDeclaration(node, code)
   node.each do |child|
-    puts "compound_statement's child node type: #{child.type}"
+    if child.type == :primitive_type
+      puts "      variable type: #{code[child.start_byte...child.end_byte]}"
+    elsif child.type == :init_declarator
+      declarator =  child.child_by_field_name('declarator')
+      puts "      variable name: #{code[declarator.start_byte...declarator.end_byte]}"
+    end 
+  end
+end
+
+# 2nd depth
+# check child node of :compound_statement
+def searchCompoundStatement(node, code)
+  node.each do |child|
+    puts "    compound_statement's child node type: #{child.type}"
     if child.type == :declaration
-      child.declarator
+      searchDeclaration(child, code)
     end
   end
 end
 
+# 1st depth
 # check child node of :function_children
-def searchFunction(node)
+def searchFunction(node, code)
   node.each do |child|
-    puts "Function's child node type: #{child.type}"
+    puts "  Function's child node type: #{child.type}"
     if child.type == :compound_statement
-      searchCompoundStatement(child)
+      searchCompoundStatement(child, code)
     end
   end
 end
@@ -36,13 +52,15 @@ src = File.read('source.c');
 # parse
 tree = parser.parse_string(nil, src)
 root = tree.root_node
+puts "==================================="
+puts root
+puts "==================================="
 
 # check children nodes of root
 root.each do |child|
   puts "child node type: #{child.type}"
   if child.type == :function_definition
-    puts ''
-    searchFunction(child)
+    searchFunction(child, src)
   end
 end
 
